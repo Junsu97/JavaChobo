9292b6e48fe7320076f7b3b5355228559b6fb0fd
Junsu97 <87612274+Junsu97@users.noreply.github.com>
2023-04-07 21:17:50 +0900

    23_04_07 자바의 정석 배열 예제

---
+++ src/main/java/Ch5/ArrayEx16.java
@@ -0,0 +1,15 @@
+package Ch5;
+
+import javax.swing.*;
+
+public class ArrayEx16 {
+    public static void main(String[] args) {
+        // 1~100사이의 임의의 값을 얻어서 answer에 저장한다.
+        int answer = (int)(Math.random() * 100) +1;
+        int input = 0;
+        String temp = "";
+        int count = 0;
+
+
+    }
+}
